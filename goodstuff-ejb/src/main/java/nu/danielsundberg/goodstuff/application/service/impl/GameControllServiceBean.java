93b73fa6035c0631a37430af405e12769b828209
Daniel Sundberg <daniel@danielsundberg.nu>
2012-10-07 21:18:16 +0200

    Fixing persistenceunit/datasource setup.

--- goodstuff-ejb/src/main/java/nu/danielsundberg/goodstuff/application/service/impl/GameControllServiceBean.java
+++ goodstuff-ejb/src/main/java/nu/danielsundberg/goodstuff/application/service/impl/GameControllServiceBean.java
@@ -16,7 +16,7 @@ import nu.danielsundberg.goodstuff.application.service.GameControllService;
 @Stateless
 public class GameControllServiceBean implements GameControllService {
 
-	@PersistenceContext(unitName = "games")
+	@PersistenceContext(unitName = "goodstuff")
     private EntityManager entityManager;
     
 	public void setEntityManager(final EntityManager entityManager) {
