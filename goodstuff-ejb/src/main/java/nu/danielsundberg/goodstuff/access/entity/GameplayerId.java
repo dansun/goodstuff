05f2752b2564acea76a481189742ff029e2288e3
Daniel Sundberg <daniel@danielsundberg.nu>
2012-10-09 20:04:44 +0200

    Implementing hashcode and equals...

--- goodstuff-ejb/src/main/java/nu/danielsundberg/goodstuff/access/entity/GameplayerId.java
+++ goodstuff-ejb/src/main/java/nu/danielsundberg/goodstuff/access/entity/GameplayerId.java
@@ -25,6 +25,30 @@ public class GameplayerId {
 	public long getPlayerId() {
 		return playerId;
 	}
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (gameId ^ (gameId >>> 32));
+		result = prime * result + (int) (playerId ^ (playerId >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GameplayerId other = (GameplayerId) obj;
+		if (gameId != other.gameId)
+			return false;
+		if (playerId != other.playerId)
+			return false;
+		return true;
+	}
 	
 }
